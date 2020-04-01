ad2e7bde836e3585e6a2a29176dffcd7754164b7
9831109 <9831109@ceit.local>
2020-04-01 14:03:06 +0430

    Overall structure of othello implemented

--- Board.java
+++ Board.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+import java.io.IOException;
 
 enum Turn {
     BLACK, WHITE;
@@ -17,10 +19,17 @@ public class Board {
      */
     private char whiteCh = '\u25CF';
     private char blackCh = '\u25CB';
-    private char turnCh = '\u25CC';
+    private char turnCh = '\u25CE';
+    private int nWhite = 0;
+    private int nBlack = 0;
     private int turn = Turn.BLACK.ordinal();
-    private ArrayList<String> blackPossibleMoves = new ArrayList<>();
-    private ArrayList<String> whitePossibleMoves = new ArrayList<>();
+    char[] turns = new char[2];
+    {
+        turns[0] = blackCh;
+        turns[1] = whiteCh;
+    }
+    String[] playerNames = new String[2];
+    private ArrayList<String> possibleMoves = new ArrayList<>();
     private char[][] board = new char[8][8];
     {
         for (int i = 0; i < 8; i++) {
@@ -34,10 +43,19 @@ public class Board {
         board[4][4] = whiteCh;
     }
 
+    public Board(String playerOneName, String playerTwoName) {
+        playerNames[0] = playerOneName;
+        playerNames[1] = playerTwoName;
+    }
+
     /**
      * shows current state of board
      */
     public void showBoard() {
+        determinePossibleMoves();
+        updateNumberOfDiscs();
+        System.out.println("White: " + nWhite + "          Black: " + nBlack);
+        System.out.println("--------------------------");
         System.out.print("  ");
         for (int i = 0; i < 8; i++) {
             System.out.print(" " + (char) (i + 65) + " ");
@@ -53,24 +71,24 @@ public class Board {
     }
 
     /**
-     * converts input of player to a standard format e.g. "A 1" to "00"
+     * converts input of player to a standard format e.g. "1 A" to "00"
      * 
-     * @param str player input like "B 4"
+     * @param str player input like "4 B"
      * @return formated stirng
      */
     public String toStandardFormat(String str) {
-        return String.valueOf(new char[] { (char) (str.charAt(0) - 17), (char) (str.charAt(2) - 1) });
+        return String.valueOf(new char[] { (char) (str.charAt(0) - 1), (char) (str.charAt(2) - 17) });
     }
 
     /**
      * checks if
      * 
-     * @param str player input like "B 4"
+     * @param str player input like "4 B"
      * @return true if input is valid
      */
     public boolean isValidInput(String str) {
-        if (65 <= (int) str.charAt(0) && (int) str.charAt(0) <= 72 && 49 <= (int) str.charAt(2)
-                && (int) str.charAt(2) <= 56) {
+        if (str.length() == 3 && 65 <= (int) str.charAt(2) && (int) str.charAt(2) <= 72 && 49 <= (int) str.charAt(0)
+                && (int) str.charAt(0) <= 56 && str.charAt(1) == ' ') {
             return true;
         }
         return false;
@@ -83,19 +101,17 @@ public class Board {
         ArrayList<String> tmp = new ArrayList<>();
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                if (board[i][j] != ' ')
+                if (board[i][j] == blackCh || board[i][j] == whiteCh)
                     continue;
-                if (isPossibleMove(i, j)) {
+                if (board[i][j] == turnCh) {
+                    tmp.add(i + "" + j);
+                } else if (isPossibleMove(i, j)) {
                     board[i][j] = turnCh;
                     tmp.add(i + "" + j);
                 }
             }
         }
-        if (turn == Turn.BLACK.ordinal()) {
-            blackPossibleMoves = tmp;
-        } else {
-            whitePossibleMoves = tmp;
-        }
+        possibleMoves = tmp;
     }
 
     /**
@@ -106,61 +122,58 @@ public class Board {
      * @return true if its a valid move
      */
     public boolean isPossibleMove(int i, int j) {
-        char[] tmp = new char[2];
-        tmp[0] = blackCh;
-        tmp[1] = whiteCh;
-        if (isValidBlock(i - 1, j) && board[i - 1][j] == tmp[oppositeTurn(turn)]) {
+        if (isValidBlock(i - 1, j) && board[i - 1][j] == turns[oppositeTurn(turn)]) {
             for (int k = 2; k < 8; k++) {
-                if (isValidBlock(i - k, j) && board[i - k][j] == tmp[turn]) {
+                if (isValidBlock(i - k, j) && board[i - k][j] == turns[turn]) {
                     return true;
                 }
             }
         }
-        if (isValidBlock(i + 1, j) && board[i + 1][j] == tmp[oppositeTurn(turn)]) {
+        if (isValidBlock(i + 1, j) && board[i + 1][j] == turns[oppositeTurn(turn)]) {
             for (int k = 2; k < 8; k++) {
-                if (isValidBlock(i + k, j) && board[i + k][j] == tmp[turn]) {
+                if (isValidBlock(i + k, j) && board[i + k][j] == turns[turn]) {
                     return true;
                 }
             }
         }
-        if (isValidBlock(i, j - 1) && board[i][j - 1] == tmp[oppositeTurn(turn)]) {
+        if (isValidBlock(i, j - 1) && board[i][j - 1] == turns[oppositeTurn(turn)]) {
             for (int k = 2; k < 8; k++) {
-                if (isValidBlock(i, j - k) && board[i][j - k] == tmp[turn]) {
+                if (isValidBlock(i, j - k) && board[i][j - k] == turns[turn]) {
                     return true;
                 }
             }
         }
-        if (isValidBlock(i, j + 1) && board[i][j + 1] == tmp[oppositeTurn(turn)]) {
+        if (isValidBlock(i, j + 1) && board[i][j + 1] == turns[oppositeTurn(turn)]) {
             for (int k = 2; k < 8; k++) {
-                if (isValidBlock(i, j + k) && board[i][j + k] == tmp[turn]) {
+                if (isValidBlock(i, j + k) && board[i][j + k] == turns[turn]) {
                     return true;
                 }
             }
         }
-        if (isValidBlock(i - 1, j - 1) && board[i - 1][j - 1] == tmp[oppositeTurn(turn)]) {
+        if (isValidBlock(i - 1, j - 1) && board[i - 1][j - 1] == turns[oppositeTurn(turn)]) {
             for (int k = 2; k < 8; k++) {
-                if (isValidBlock(i - k, j - k) && board[i - k][j - k] == tmp[turn]) {
+                if (isValidBlock(i - k, j - k) && board[i - k][j - k] == turns[turn]) {
                     return true;
                 }
             }
         }
-        if (isValidBlock(i + 1, j + 1) && board[i + 1][j + 1] == tmp[oppositeTurn(turn)]) {
+        if (isValidBlock(i + 1, j + 1) && board[i + 1][j + 1] == turns[oppositeTurn(turn)]) {
             for (int k = 2; k < 8; k++) {
-                if (isValidBlock(i + k, j + k) && board[i + k][j + k] == tmp[turn]) {
+                if (isValidBlock(i + k, j + k) && board[i + k][j + k] == turns[turn]) {
                     return true;
                 }
             }
         }
-        if (isValidBlock(i + 1, j - 1) && board[i + 1][j - 1] == tmp[oppositeTurn(turn)]) {
+        if (isValidBlock(i + 1, j - 1) && board[i + 1][j - 1] == turns[oppositeTurn(turn)]) {
             for (int k = 2; k < 8; k++) {
-                if (isValidBlock(i + k, j - k) && board[i + k][j - k] == tmp[turn]) {
+                if (isValidBlock(i + k, j - k) && board[i + k][j - k] == turns[turn]) {
                     return true;
                 }
             }
         }
-        if (isValidBlock(i - 1, j + 1) && board[i - 1][j + 1] == tmp[oppositeTurn(turn)]) {
+        if (isValidBlock(i - 1, j + 1) && board[i - 1][j + 1] == turns[oppositeTurn(turn)]) {
             for (int k = 2; k < 8; k++) {
-                if (isValidBlock(i - k, j + k) && board[i - k][j + k] == tmp[turn]) {
+                if (isValidBlock(i - k, j + k) && board[i - k][j + k] == turns[turn]) {
                     return true;
                 }
             }
@@ -192,5 +205,161 @@ public class Board {
             return Turn.WHITE.ordinal();
         else
             return Turn.BLACK.ordinal();
+    }
+
+    /**
+     * plays a turn and changes turn
+     * 
+     * @param playerInp
+     */
+    public int playTurn(String playerInp) throws InterruptedException, IOException, InterruptedException {
+        Sleep sleep = new Sleep();
+        if (!isValidInput(playerInp)) {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            System.out.println("invalid input");
+            Thread.sleep(1500);
+            return 0;
+        }
+        String stPlayerInp = toStandardFormat(playerInp);
+        if (possibleMoves.isEmpty()) {
+            System.out.println("pass");
+            Thread.sleep(1500);
+            return 1;
+        }
+        if (possibleMoves.contains(stPlayerInp)) {
+            rotateAllInBetween(Integer.parseInt(stPlayerInp.charAt(0) + ""),
+                    Integer.parseInt(stPlayerInp.charAt(1) + ""));
+            board[Integer.parseInt(stPlayerInp.charAt(0) + "")][Integer
+                    .parseInt(stPlayerInp.charAt(1) + "")] = turns[turn];
+            turn = oppositeTurn(turn);
+            clearTurnChs();
+            return 0;
+        } else {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            System.out.println("invalid block");
+            Thread.sleep(1500);
+            return 0;
+        }
+    }
+
+    /**
+     * clears map form turn character
+     */
+    public void clearTurnChs() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == turnCh) {
+                    board[i][j] = ' ';
+                }
+            }
+        }
+    }
+
+    /**
+     * updates number of discs
+     */
+    public void updateNumberOfDiscs() {
+        int nWhiteHolder = 0;
+        int nBlackHolder = 0;
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (board[i][j] == whiteCh) {
+                    nWhiteHolder++;
+                }
+                if (board[i][j] == blackCh) {
+                    nBlackHolder++;
+                }
+            }
+        }
+        nWhite = nWhiteHolder;
+        nBlack = nBlackHolder;
+    }
+
+    /**
+     * proper massage to enter input
+     */
+    public void enterInp() {
+        System.out.print(playerNames[turn] + "'s turn: ");
+    }
+
+    /**
+     * rotates all discs between two of player's discs
+     * 
+     * @param i row index
+     * @param j columns index
+     */
+    public void rotateAllInBetween(int i, int j) {
+        if (isValidBlock(i - 1, j) && board[i - 1][j] == turns[oppositeTurn(turn)]) {
+            for (int k = 2; k < 8; k++) {
+                if (isValidBlock(i - k, j) && board[i - k][j] == turns[turn]) {
+                    for (int x = 1; x < k; x++) {
+                        board[i - x][j] = turns[turn];
+                    }
+                }
+            }
+        }
+        if (isValidBlock(i + 1, j) && board[i + 1][j] == turns[oppositeTurn(turn)]) {
+            for (int k = 2; k < 8; k++) {
+                if (isValidBlock(i + k, j) && board[i + k][j] == turns[turn]) {
+                    for (int x = 1; x < k; x++) {
+                        board[i + x][j] = turns[turn];
+                    }
+                }
+            }
+        }
+        if (isValidBlock(i, j - 1) && board[i][j - 1] == turns[oppositeTurn(turn)]) {
+            for (int k = 2; k < 8; k++) {
+                if (isValidBlock(i, j - k) && board[i][j - k] == turns[turn]) {
+                    for (int x = 1; x < k; x++) {
+                        board[i][j - x] = turns[turn];
+                    }
+                }
+            }
+        }
+        if (isValidBlock(i, j + 1) && board[i][j + 1] == turns[oppositeTurn(turn)]) {
+            for (int k = 2; k < 8; k++) {
+                if (isValidBlock(i, j + k) && board[i][j + k] == turns[turn]) {
+                    for (int x = 1; x < k; x++) {
+                        board[i][j + x] = turns[turn];
+                    }
+                }
+            }
+        }
+        if (isValidBlock(i - 1, j - 1) && board[i - 1][j - 1] == turns[oppositeTurn(turn)]) {
+            for (int k = 2; k < 8; k++) {
+                if (isValidBlock(i - k, j - k) && board[i - k][j - k] == turns[turn]) {
+                    for (int x = 1; x < k; x++) {
+                        board[i - x][j - x] = turns[turn];
+                    }
+                }
+            }
+        }
+        if (isValidBlock(i + 1, j + 1) && board[i + 1][j + 1] == turns[oppositeTurn(turn)]) {
+            for (int k = 2; k < 8; k++) {
+                if (isValidBlock(i + k, j + k) && board[i + k][j + k] == turns[turn]) {
+                    for (int x = 1; x < k; x++) {
+                        board[i + x][j + x] = turns[turn];
+                    }
+                }
+            }
+        }
+        if (isValidBlock(i + 1, j - 1) && board[i + 1][j - 1] == turns[oppositeTurn(turn)]) {
+            for (int k = 2; k < 8; k++) {
+                if (isValidBlock(i + k, j - k) && board[i + k][j - k] == turns[turn]) {
+                    for (int x = 1; x < k; x++) {
+                        board[i + x][j - x] = turns[turn];
+                    }
+                }
+            }
+        }
+        if (isValidBlock(i - 1, j + 1) && board[i - 1][j + 1] == turns[oppositeTurn(turn)]) {
+            for (int k = 2; k < 8; k++) {
+                if (isValidBlock(i - k, j + k) && board[i - k][j + k] == turns[turn]) {
+                    for (int x = 1; x < k; x++) {
+                        board[i - x][j + x] = turns[turn];
+                    }
+                }
+            }
+        }
     }
 }
--- Main.java
+++ Main.java
@@ -1,15 +1,35 @@
+import java.util.Scanner;
+import java.io.IOException;
+
 /**
  * Main
  * 
  * @author Amirali Belbasi
  */
 public class Main {
-
-    public static void main(String[] args) {
-        Board test = new Board();
-        test.showBoard();
-        System.out.println("-----");
-        test.determinePossibleMoves();
-        test.showBoard();
+    /**
+     * Code: new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+     * is a way to clean console & was copied from
+     * 
+     * @see https://stackoverflow.com/questions/2979383/java-clear-the-console
+     * @param arg
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static void main(String... arg) throws IOException, InterruptedException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        Scanner sc = new Scanner(System.in);
+        Board test = new Board("Mr.Black", "Mr.White");
+        int passCounter = 0;
+        while (true) {
+            if (passCounter == 2)
+                break;
+            test.showBoard();
+            test.enterInp();
+            String inp = sc.nextLine();
+            passCounter += test.playTurn(inp);
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        }
+        sc.close();
     }
 }
