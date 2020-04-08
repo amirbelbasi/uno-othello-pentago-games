29fe0608a72f18ff4a68b5763d1da7022a7b4c19
9831109 <9831109@ceit.local>
2020-04-08 08:56:09 +0430

    Pentago single player implemented

--- Pentago/Board.java
+++ Pentago/Board.java
@@ -1,4 +1,6 @@
+import java.util.Random;
 import java.util.Scanner;
+import java.io.IOException;
 
 /**
  * provides more readability (hopefully!)
@@ -6,6 +8,7 @@ import java.util.Scanner;
 enum Turn {
     BLACK, WHITE;
 }
+
 /**
  * class to represent board of othello
  * 
@@ -19,80 +22,101 @@ public class Board {
      */
     private char whiteCh = '\u25CF';
     private char blackCh = '\u25CB';
-    private char emptyCh = '\u25CE';
+    private char emptyCh = '\u25A1';
     private static char[][] board = new char[6][6];
     {
-        for (int i = 0; i < 6; i++)
-        {
-            for (int j = 0; j < 6; j++)
-            {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
                 board[i][j] = emptyCh;
             }
         }
     }
     private int turn = Turn.BLACK.ordinal();
+    private boolean hasPutDisc = false; // determines if player has puted his disc
+    private boolean isBlackWinner = false;
+    private boolean isWhiteWinner = false;
+    private String player1, player2;
+    private String[] playerNames = new String[2];
+
+    /**
+     * constructor
+     * 
+     * @param blackPlayer name of black player
+     * @param whitePlayer name of white player
+     */
+    public Board(String player1, String player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+        Random rnd = new Random();
+        int randIndex = rnd.nextInt(2);
+        playerNames[randIndex] = player1;
+        playerNames[randIndex == 0 ? 1 : 0] = player2;
+    }
 
     /**
      * shows board
-     * @throws InterruptedException
+     * 
      * @throws IOException
      */
-    public void showBoard() throws InterruptedException, IOException
-    {
+    private void showBoard() throws InterruptedException, IOException {
         new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-        for (int i = 0; i < 6; i++)
-        {
-            if (i == 3)
-            {
+        for (int i = 0; i < 6; i++) {
+            if (i == 3) {
                 System.out.println("-------------");
             }
-            for (int j = 0; j < 6; j++)
-            {
-                if (j == 3)
-                {
-                    System.out.print("|");
+            for (int j = 0; j < 6; j++) {
+                if (j == 3) {
+                    System.out.print("| ");
                 }
                 System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println();
     }
 
     /**
      * rotates a block clock wise or counter clock wise
+     * 
      * @param indexOfBlock 1, 2, 3 or 4
-     * @param direction CW or CCW
+     * @param direction    + or -
      */
-    private void rotate(int indexOfBlock, String direction)
-    {
+    private void rotate(int indexOfBlock, String direction) {
         char[][] holderBoard = new char[6][6];
         int jShift = 0;
         int iShift = 0;
-        if(indexOfBlock > 2)
-        {
+        if (indexOfBlock > 2) {
             iShift = 3;
         }
-        if (indexOfBlock % 2 == 0)
-        {
+        if (indexOfBlock % 2 == 0) {
             jShift = 3;
         }
-        if (direction.equals("CW")) // clock wise
+        if (direction.equals("+")) // clock wise
         {
-            for (int a = 0; a < 3; a++)
-            {
-                for (int b = 0; b < 3; b++)
-                {
+            for (int a = 0; a < 3; a++) {
+                for (int b = 0; b < 3; b++) {
                     holderBoard[a + iShift][b + jShift] = board[2 - b + iShift][a + jShift];
                 }
             }
-        }
-        else // counter clock wise
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 6; j++) {
+                    if (holderBoard[i][j] == '\0') {
+                        holderBoard[i][j] = board[i][j];
+                    }
+                }
+            }
+        } else // counter clock wise
         {
-            for (int a = 0; a < 3; a++)
-            {
-                for (int b = 0; b < 3; b++)
-                {
+            for (int a = 0; a < 3; a++) {
+                for (int b = 0; b < 3; b++) {
                     holderBoard[2 - b + iShift][a + jShift] = board[a + iShift][b + jShift];
+                }
+            }
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 6; j++) {
+                    if (holderBoard[i][j] == '\0') {
+                        holderBoard[i][j] = board[i][j];
+                    }
                 }
             }
         }
@@ -101,60 +125,85 @@ public class Board {
 
     /**
      * player playing his turn
+     * 
      * @throws InterruptedException
      * @throws IOException
      */
-    public void playTurn() throws InterruptedException, IOException
-    {
+    public int playTurn() throws InterruptedException, IOException {
         Scanner sc = new Scanner(System.in);
-        System.out.print(turn == Turn.BLACK.ordinal() ? Turn.BLACK : Turn.WHITE + "'s turn ");
-        System.out.print("please enter coordinates of cell you wish to put your disc on: ");
-        String inp = sc.nextLine();
-        if (!isInputValid(inp))
-        {
+        if (isBoardFull()) {
+            determineWinnner();
+            return -1;
+        }
+        if (!hasPutDisc) {
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            showBoard();
+            System.out.println(playerNames[turn] + "'s turn ");
+            System.out.print("please enter coordinates of cell you wish to put your disc on: ");
+            String cellInp = sc.nextLine();
+            if (!isCoordinatesOfCellValid(cellInp)) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                System.out.println("invalid input");
+                Thread.sleep(1500);
+                return 0;
+            }
+            if (!isCellEmpty(cellInp)) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+                System.out.println("invalid cell");
+                Thread.sleep(1500);
+                return 0;
+            }
+            board[Integer.parseInt(cellInp.charAt(0) + "")][Integer
+                    .parseInt(cellInp.charAt(2) + "")] = (turn == Turn.BLACK.ordinal() ? blackCh : whiteCh);
+            hasPutDisc = true;
+            if (isThereAWinner()) {
+                determineWinnner();
+                return -1;
+            }
+        }
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        showBoard();
+        System.out.println(playerNames[turn] + "'s turn ");
+        System.out.print("please enter details of block you wish to rotate: ");
+        String blockInp = sc.nextLine();
+        if (!isDetailsOfBlockValid(blockInp)) {
             new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
             System.out.println("invalid input");
             Thread.sleep(1500);
-            return;
+            return 0;
         }
-        if (!isValidCell(inp))
-        {
-            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            System.out.println("invalid cell");
-            Thread.sleep(1500);
-            return;
-        }
-        board[Integer.parseInt(inp.charAt(0)+"")][Integer.parseInt(inp.charAt(2)+"")] = (turn == Turn.BLACK.ordinal() ? blackCh : whiteCh);
-        System.out.print("please enter details of block you wish to rotate: ");
-        inp = sc.nextLine();
-        rotate(indexOfBlock, direction);
-
+        rotate(Integer.parseInt(blockInp.charAt(0) + ""), blockInp.charAt(2) + "");
         turn = switchTurn(turn);
+        hasPutDisc = false;
+        if (isThereAWinner()) {
+            determineWinnner();
+            return -1;
+        }
+        return 0;
     }
 
     /**
      * returns opposite turn
+     * 
      * @param currentTurn
      * @return
      */
-    private int switchTurn(int currentTurn)
-    {
+    private int switchTurn(int currentTurn) {
         return currentTurn == Turn.BLACK.ordinal() ? Turn.WHITE.ordinal() : Turn.BLACK.ordinal();
     }
 
     /**
      * checks if input is valid
+     * 
      * @param inp
      * @return true if input is valid
      */
-    private boolean isInputValid(String inp)
-    {
-        if (inp.length() != 3)
-        {
+    private boolean isCoordinatesOfCellValid(String inp) {
+        if (inp.length() != 3) {
             return false;
         }
-        if ((int)inp.charAt(0) - 48 >= 0 && (int)inp.charAt(0) - 48 <= 5 && (int)inp.charAt(2) - 48 >= 0 && (int)inp.charAt(2) - 48 <= 5)
-        {
+        if ((int) inp.charAt(0) - 48 >= 0 && (int) inp.charAt(0) - 48 <= 5 && (int) inp.charAt(2) - 48 >= 0
+                && (int) inp.charAt(2) - 48 <= 5) {
             return true;
         }
         return false;
@@ -162,15 +211,150 @@ public class Board {
 
     /**
      * checks if block is empty
+     * 
      * @param inp
      * @return ture if cell is empty
      */
-    private boolean isValidCell(String inp)
-    {
-        if(board[Integer.parseInt(inp.charAt(0)+"")][Integer.parseInt(inp.charAt(2)+"")] != emptyCh)
-        {
+    private boolean isCellEmpty(String inp) {
+        if (board[Integer.parseInt(inp.charAt(0) + "")][Integer.parseInt(inp.charAt(2) + "")] != emptyCh) {
             return false;
         }
         return true;
+    }
+
+    /**
+     * checks if given block detail is valid
+     * 
+     * @param inp
+     * @return
+     */
+    private boolean isDetailsOfBlockValid(String inp) {
+        if (inp.length() != 3) {
+            return false;
+        }
+        if ((int) inp.charAt(0) - 48 >= 1 && (int) inp.charAt(0) - 48 <= 4
+                && (inp.charAt(2) == '+' || inp.charAt(2) == '-')) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * checks if board is full
+     * 
+     * @return true if board is full
+     */
+    private boolean isBoardFull() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (board[i][j] == emptyCh) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
+     * checks if there is a winner
+     * 
+     * @return true if there is a winner
+     */
+    private boolean isThereAWinner() {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (board[i][j] == emptyCh) {
+                    continue;
+                }
+                int counter = 1;
+                for (int k = 1; k < 6; k++) {
+                    if (isCoordinatesOfCellValid((i + k) + " " + j) && board[i + k][j] == board[i][j]) {
+                        counter++;
+                    }
+                    if (isCoordinatesOfCellValid((i - k) + " " + j) && board[i - k][j] == board[i][j]) {
+                        counter++;
+                    }
+                }
+                if (counter == 5) {
+                    if (board[i][j] == blackCh) {
+                        isBlackWinner = true;
+                    } else {
+                        isWhiteWinner = true;
+                    }
+                }
+                counter = 1;
+                for (int k = 1; k < 6; k++) {
+                    if (isCoordinatesOfCellValid(i + " " + (j + k)) && board[i][j + k] == board[i][j]) {
+                        counter++;
+                    }
+                    if (isCoordinatesOfCellValid(i + " " + (j - k)) && board[i][j - k] == board[i][j]) {
+                        counter++;
+                    }
+                }
+                if (counter == 5) {
+                    if (board[i][j] == blackCh) {
+                        isBlackWinner = true;
+                    } else {
+                        isWhiteWinner = true;
+                    }
+                }
+                counter = 1;
+                for (int k = 1; k < 6; k++) {
+                    if (isCoordinatesOfCellValid((i + k) + " " + (j + k)) && board[i + k][j + k] == board[i][j]) {
+                        counter++;
+                    }
+                    if (isCoordinatesOfCellValid((i - k) + " " + (j - k)) && board[i - k][j - k] == board[i][j]) {
+                        counter++;
+                    }
+                }
+                if (counter == 5) {
+                    if (board[i][j] == blackCh) {
+                        isBlackWinner = true;
+                    } else {
+                        isWhiteWinner = true;
+                    }
+                }
+                counter = 1;
+                for (int k = 1; k < 6; k++) {
+                    if (isCoordinatesOfCellValid((i + k) + " " + (j - k)) && board[i + k][j - k] == board[i][j]) {
+                        counter++;
+                    }
+                    if (isCoordinatesOfCellValid((i - k) + " " + (j + k)) && board[i - k][j + k] == board[i][j]) {
+                        counter++;
+                    }
+                }
+                if (counter == 5) {
+                    if (board[i][j] == blackCh) {
+                        isBlackWinner = true;
+                    } else {
+                        isWhiteWinner = true;
+                    }
+                }
+            }
+        }
+        return isBlackWinner || isWhiteWinner ? true : false;
+    }
+
+    /**
+     * determines the winner
+     * 
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    private void determineWinnner() throws InterruptedException, IOException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        showBoard();
+        if ((isBlackWinner && isWhiteWinner) || (!isBlackWinner && !isWhiteWinner)) {
+            System.out.print("Tie!");
+            return;
+        }
+        if (isBlackWinner) {
+            System.out.println("The winner is: " + playerNames[0]);
+            return;
+        }
+        if (isWhiteWinner) {
+            System.out.println("The winner is: " + playerNames[1]);
+            return;
+        }
     }
 }
--- Pentago/Main.java
+++ Pentago/Main.java
@@ -1,22 +1,38 @@
 import java.util.Scanner;
+import java.io.IOException;
+// import org.fusesource.jansi.AnsiConsole;
+// import static org.fusesource.jansi.Ansi.*;
+// import static org.fusesource.jansi.Ansi.Color.*;
 
 /**
  * Main
  */
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
-        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        // AnsiConsole.systemInstall();
+        // AnsiConsole.systemUninstall();
+        // System.out.println( ansi().eraseScreen().render("@|red Hello|@ @|green World|@") );
         Scanner sc = new Scanner(System.in);
-        Board test = new Board();
+        String player1, player2;
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        System.out.print("enter player1 name: ");
+        player1 = sc.nextLine();
+        System.out.print("enter player2 name: ");
+        player2 = sc.nextLine();
+        Board test = new Board(player1, player2);
         /**
+         * READ ME PLEASE
          * input guide:
-         * coordinates of cell should be like "<index of row> <index of column>" like "2 0"
-         * details of block should be like ""
+         * coordinates of cell should be like "<index of row> <index of column>" e.g. "5 0" would be left down corner
+         * & "0 5 would be right up corner"
+         * details of block should be like "<index of block> <direction>" e.g. "1 +", "2 -" or "4 +"
+         * + ==> clock wise
+         * - ==> counter clock wise
          */
-        while (true)
-        {
-            test.showBoard();
-            test.playTurn();
+        while (true) {
+            if (test.playTurn() == -1) {
+                break;
+            }
         }
     }
 }
